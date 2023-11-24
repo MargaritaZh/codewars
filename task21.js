888cfd3579a8b07caf63434acf2c5065dcb353cd
MargaritaZh <margaritazhitkevich@gmail.com>
2023-11-25 00:35:32 +0300

    Enumerable Magic #20 - Cascading Subsets

---
+++ task21.js
@@ -0,0 +1,22 @@
+//Enumerable Magic #20 - Cascading Subsets
+/*Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
+
+each_cons([1,2,3,4], 2)
+  #=> [[1,2], [2,3], [3,4]]
+
+each_cons([1,2,3,4], 3)
+  #=> [[1,2,3],[2,3,4]]
+  */
+
+function eachCons(array, n) {
+  const r = [];
+  for (let i = 0; i <= array.length - n; i++) {
+    const chunk = [];
+    for (let j = i; j < i + n; j++) {
+      chunk.push(array[j]);
+    }
+    r.push(chunk);
+  }
+
+  return r;
+}
