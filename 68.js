0ac38747181d843c5ece1a7ee2af7f8e3065e052
MargaritaZh <margaritazhitkevich@gmail.com>
2024-02-01 23:05:40 +0300

    Beginner - Reduce but Grow

---
+++ 68.js
@@ -0,0 +1,23 @@
+//Beginner - Reduce but Grow
+// Если задан непустой массив целых чисел, верните результат умножения их значений по порядку. Пример:
+//
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// assert.strictEqual(grow([1, 2, 3]), 6);
+// assert.strictEqual(grow([4, 1, 1, 1, 4]), 16);
+// assert.strictEqual(grow([2, 2, 2, 2, 2, 2]), 64);
+
+function grow(x){
+    if(x.length!==0){
+        let res = 1
+        for(let i=0; i< x.length; i++){
+            res *=x[i]
+        }
+        return res
+    }
+}
+
+
+function grow2(x){
+    return x.reduce((a, b)=> a * b,1);
+}
