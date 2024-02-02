1764ac0cbb558525da57685797df900c617701b5
MargaritaZh <margaritazhitkevich@gmail.com>
2024-02-02 23:35:04 +0300

    Sum Mixed Array

---
+++ 74.js
@@ -0,0 +1,20 @@
+//Sum Mixed Array
+// учитывая массив целых чисел в виде строк и чисел, верните сумму значений массива, как если бы все они были числами.
+// Верните ответ в виде числа.
+
+
+function sumMix(x){
+    return x.map(a => +a).reduce((a, b) => a + b);
+}
+
+
+function sumMix(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let element = parseFloat(arr[i]);
+        if (!isNaN(element)) {
+            sum += element;
+        }
+    }
+    return sum;
+}
