1bd214e66b58bc712900da9f737a95827c4ebfcb
MargaritaZh <margaritazhitkevich@gmail.com>
2024-02-02 23:36:37 +0300

    Sum Mixed Array

--- 74.js
+++ 74.js
@@ -1,7 +1,9 @@
 //Sum Mixed Array
 // учитывая массив целых чисел в виде строк и чисел, верните сумму значений массива, как если бы все они были числами.
 // Верните ответ в виде числа.
-
+// assert.strictEqual(sumMix([9, 3, '7', '3']), 22);
+// assert.strictEqual(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42);
+// assert.strictEqual(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41);
 
 function sumMix(x){
     return x.map(a => +a).reduce((a, b) => a + b);
