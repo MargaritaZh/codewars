8898bc3ca270a0ca816189816c8619b610d49b77
MargaritaZh <margaritazhitkevich@gmail.com>
2024-02-04 15:03:28 +0300

    Convert a string to an array

---
+++ 86.Convert a string to an array.js
@@ -0,0 +1,9 @@
+//Convert a string to an array
+// Напишите функцию для разбиения строки и преобразования ее в массив слов.
+
+// "Robin Singh" ==> ["Robin", "Singh"]
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string){
+ return string.split(" ")
+}
